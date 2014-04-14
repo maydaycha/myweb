41a49b52c77a1fc9e8519f3ccfd58efb9ade755d
maydaycha <maydaychaaaa@gmail.com>
2014-04-15 00:47:45 +0800

    goole

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -26,7 +26,6 @@ class Users::OmniauthCallbacksController < ApplicationController
       session["devise:token"] = request.env["omniauth.auth"]["credentials"]["token"]
       redirect_to new2_user_registration_path
 
-
       # user = User.new
       # user.first_name = omni['extra']['raw_info'].first_name
       # user.last_name = omni['extra']['raw_info'].last_name
@@ -46,6 +45,28 @@ class Users::OmniauthCallbacksController < ApplicationController
   end
 
   def google_oauth2
-    return render :json => { content: request.env["omniauth.auth"] }
+    omni = request.env["omniauth.auth"]
+
+    user_authentication = UserAuthentication.find_by_provider_and_uid(omni['provider'], omni['uid'])
+
+    if user_authentication
+      flash[:notice] = "Logged in Successfully"
+      sign_in_and_redirect User.find(user_authentication.user_id)
+    elsif current_user
+      token = omni['credentials'].token
+      token_secret = ""
+
+      current_user.user_authentications.create!(:provider => omni['provider'], :uid => omni['uid'], :token => token, :token_secret => token_secret)
+
+      flash[:notice] = "Authentication successful."
+      sign_in_and_redirect current_user
+    else
+      session["devise:provider"] = request.env["omniauth.auth"]["provider"]
+      session["devise:uid"] = request.env["omniauth.auth"]["uid"]
+      session["devise:info"] = request.env["omniauth.auth"]["info"]
+      session["devise:token"] = request.env["omniauth.auth"]["credentials"]["token"]
+      redirect_to new2_user_registration_path
+    end
   end
+
 end
