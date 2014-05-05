a3108082eeefc23451fedbff09b0557ead488167
maydaycha <maydaychaaaa@gmail.com>
2014-05-05 14:12:41 +0800

    heroku

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,36 +4,46 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def create
     if verify_recaptcha
     # if true
-      super
-      session[:omniauth] = nil unless @user.new_record?
-      @user.user_authentications.create!(:provider => session["devise:provider"], :uid => session['devise:uid'], :token => session["devise:token"], :token_secret => "") if session["devise:provider"] != nil
-    else
-      build_resource(sign_up_params)
-      clean_up_passwords(resource)
-      flash.now[:alert] = "There was an error with the recaptcha code below. Please re-enter the code."
-      flash.delete :recaptcha_error
-      render :new
-    end
-  end
-
-  def verify_email
-    print "======="
-    print params.to_json
-    print "======="
-    @email = params[:email]
-    render template: "users/registrations/verify_email"
-  end
-
-  def after_inactive_sign_up_path_for(user)
-    verify_user_registration_path(email: user.email)
-  end
-
-  def build_resource(*args)
     super
+    session[:omniauth] = nil unless @user.new_record?
+    @user.user_authentications.create!(:provider => session["devise:provider"], :uid => session['devise:uid'], :token => session["devise:token"], :token_secret => "") if session["devise:provider"] != nil
+  else
+    build_resource(sign_up_params)
+    clean_up_passwords(resource)
+    flash.now[:alert] = "There was an error with the recaptcha code below. Please re-enter the code."
+    flash.delete :recaptcha_error
+    render :new
+  end
+end
+
+def verify_email
+  print "======="
+  print params.to_json
+  print "======="
+  @email = params[:email]
+  render template: "users/registrations/verify_email"
+end
+
+def after_inactive_sign_up_path_for(user)
+  verify_user_registration_path(email: user.email)
+end
+
+def build_resource(*args)
+  super
     # if session[:omniauth]
     #   @user.apply_omniauth(session[:omniauth])
     #   @user.valid?
     # end
+  end
+
+  def new
+    if params[:username]
+      build_resource({
+        username: params[:username]
+        })
+    else
+      super
+    end
   end
 
   def new2
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -2,7 +2,7 @@ require 'spreadsheet'
 
 class Skill < ActiveRecord::Base
   def self.import_from_csv
-    book = Spreadsheet.open "#{Rails.root}/public/20140428_skill.xls"
+    book = Spreadsheet.open "#{Rails.root}/db/20140428_skill.xls"
     sheet1 = book.worksheet 0
     sheet1.each_with_index do |row, index|
       next if index < 2
