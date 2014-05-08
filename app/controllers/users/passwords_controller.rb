69bb46f99fa888e4d821cd156053fd00c2fa2c91
Scott Chou <scott03333@gmail.com>
2014-05-09 00:05:22 +0800

    bug fixed ppt p18

---
+++ app/controllers/users/passwords_controller.rb
@@ -0,0 +1,12 @@
+class Users::PasswordsController < Devise::PasswordsController
+  def edit
+  	super
+  	original_token       = params[:reset_password_token]
+    reset_password_token = Devise.token_generator.digest(self, :reset_password_token, original_token)
+  	user = User.find_by_reset_password_token(reset_password_token)
+  	
+  	if user
+  		resource.username = user.username
+  	end
+  end
+end
