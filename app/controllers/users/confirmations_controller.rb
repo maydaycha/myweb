6a76f112a2d807f72cb7c95d1879eed0e767ecfd
Scott Chou <scott03333@gmail.com>
2014-05-16 14:25:44 +0800

    bug fixed 9, 10, 11

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -14,6 +14,19 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
     end
   end
 
+  # GET /resource/confirmation?confirmation_token=abcdef
+  def show
+    self.resource = resource_class.confirm_by_token(params[:confirmation_token])
+    yield resource if block_given?
+
+    if resource.errors.empty?
+      set_flash_message(:notice, :confirmed) if is_flashing_format?
+      respond_with_navigational(resource){ redirect_to after_confirmation_path_for(resource_name, resource) }
+    else
+      respond_with_navigational(resource.errors, status: :unprocessable_entity){ redirect_to after_confirmation_path_for(resource_name, resource) }
+    end
+  end
+
   def after_resending_confirmation_instructions_path_for(resource_name)
   	verify_user_registration_path(email: self.resource.email) if is_navigational_format?
   end
