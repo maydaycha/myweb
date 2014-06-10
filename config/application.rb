79126d25de53bff4666ceea63bf5d2075d1048a1
maydaycha <maydaychaaaa@gmail.com>
2014-06-10 14:10:48 +0800

    debug active admin

--- config/application.rb
+++ config/application.rb
@@ -25,16 +25,5 @@ module Sun
     config.encoding = "utf-8"
 
     config.assets.initialize_on_precompile = false
-
-    # load ENV
-    config.before_configuration do
-      env_file = Rails.root.join("config", 'environment_variables.yml').to_s
-
-      if File.exists?(env_file)
-        YAML.load_file(env_file)[Rails.env].each do |key, value|
-          ENV[key.to_s] = value
-        end # end YAML.load_file
-      end # end if File.exists?
-    end # end config.before_configuration
   end
 end
