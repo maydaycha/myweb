2e22f6a153accfd3ff53fdb2c0dce82356d8bd7f
flyingapsara <flyingapsara@admindeMac-mini.local>
2014-10-23 17:13:01 +0800

    將projects的main_skill和sub_skill改成了integer type 並且一併修改了projects 的 show page 還有projects/search 的skill 搜尋功能

---
+++ db/migrate/20141023085416_change_projects_skills_type.rb
@@ -0,0 +1,13 @@
+class ChangeProjectsSkillsType < ActiveRecord::Migration
+  def change
+  	def up
+    change_column :projects, :main_skill, :integer
+  	change_column :projects, :sub_skill, :integer
+  end
+
+  def down
+  	change_column :projects, :main_skill, :string
+  	change_column :projects, :sub_skill, :string
+  end
+  end
+end
