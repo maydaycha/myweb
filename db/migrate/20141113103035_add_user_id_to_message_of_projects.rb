cc9b29e80f9b5a477e67ccc70c19dc97dde076a8
maydaycha <maydaychaaaa@gmail.com>
2014-11-20 13:55:52 +0800

    migrate bug fix

--- db/migrate/20141113103035_add_user_id_to_message_of_projects.rb
+++ db/migrate/20141113103035_add_user_id_to_message_of_projects.rb
@@ -1,5 +1,5 @@
 class AddUserIdToMessageOfProjects < ActiveRecord::Migration
   def change
-  	add_column :message_of_projects, :user_id, :integer
+  	add_column :messages_of_projects, :user_id, :integer
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141112081155) do
+ActiveRecord::Schema.define(version: 20141113103035) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 20141112081155) do
     t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "oauths", force: true do |t|
